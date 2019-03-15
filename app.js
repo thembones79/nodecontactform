c11483042880f385d0965f7d09c6c3d88ca87c7c
Michał Szumnarski <michal.szumnarski@gmail.com>
2019-03-15 11:11:35 +0100

    it works :)

--- app.js
+++ app.js
@@ -21,14 +21,75 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
 // Body Parser Middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('contact');
 });
 
-app.listen(3000, ()=> console.log('Server started...'));
+
+app.post('/send', (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact details</h3>
+    <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Company: ${req.body.company}</li>
+    <li>Email: ${req.body.email}</li>
+    <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+    `;
+
+
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.office365.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "order@unisystem.pl", // generated ethereal user
+            pass: "turlam3M&Msy" // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Kontakt z formularza 👻" <order@unisystem.pl>', // sender address
+        to: "michal@unisystem.pl, michal.szumnarski@gmail.com", // list of receivers
+        subject: "Kontakt z formularza ✔", // Subject line
+        text: "Turn on HTML in your mail client", // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+res.render('contact', {msg: 'Email has been sent'});
+
+
+    });
+
+
+
+
+});
+
+
+app.listen(3000, () => console.log('Server started...'));
