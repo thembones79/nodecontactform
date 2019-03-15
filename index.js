b1f27b7b66e730173167c162f82bce2fb0e02f5e
Michał Szumnarski <michal.szumnarski@gmail.com>
2019-03-15 14:57:18 +0100

    added test

--- index.js
+++ index.js
@@ -15,23 +15,23 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
-  const count = 5;
+    const count = 5;
 
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
+    // Generate some passwords
+    const passwords = Array.from(Array(count).keys()).map(i =>
+        generatePassword(12, false)
+    )
 
-  // Return them as json
-  res.json(passwords);
+    // Return them as json
+    res.json(passwords);
 
-  console.log(`Sent ${count} passwords`);
+    console.log(`Sent ${count} passwords`);
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 
@@ -97,7 +97,23 @@ app.post('/send', (req, res) => {
         console.log("Preview URL is: %s", nodemailer.getTestMessageUrl(info));
         //res.json({msg: 'Email has been sent'});
         //res.sendFile(path.join(__dirname+'/client/build/index.html'));
-        res.redirect("https://www.unisystem.pl/pl/thankyou");
+        //res.redirect("https://www.unisystem.pl/pl/thankyou");
+
+
+
+
+        var send = req.param("send");
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write('Code: ' + code);
+        res.write('<script>setTimeout(function () { window.location.href = "https://www.unisystem.pl/pl/test_form"; }, 5000);</script>');
+        res.end();
+
+
+
+
 
 
 
