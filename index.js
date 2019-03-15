d4eac17faf87dee872c996e60c66b6c44c44e940
Michał Szumnarski <michal.szumnarski@gmail.com>
2019-03-15 12:23:31 +0100

    added ghost

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+
+
+
 
 const app = express();
 
@@ -27,6 +33,98 @@ app.get('/api/passwords', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+
+
+
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+
+
+
+
+
+app.post('/send', (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact details</h3>
+    <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Company: ${req.body.company}</li>
+    <li>Email: ${req.body.email}</li>
+    <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+    `;
+
+
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.office365.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "order@unisystem.pl", // generated ethereal user
+            pass: "turlam3M&Msy" // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Kontakt z formularza 👻" <order@unisystem.pl>', // sender address
+        to: "michal@unisystem.pl, michal.szumnarski@gmail.com", // list of receivers
+        subject: "Kontakt z formularza ✔", // Subject line
+        text: "Turn on HTML in your mail client", // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL is: %s", nodemailer.getTestMessageUrl(info));
+        //res.json({msg: 'Email has been sent'});
+
+
+
+    });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
