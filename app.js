745961a035158ffc19014cd1c02aef1b0e1aedeb
Michał Szumnarski <michal.szumnarski@gmail.com>
2019-03-15 15:34:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post('/send', (req, res) => {
         secure: false, // true for 465, false for other ports
         auth: {
             user: "order@unisystem.pl", // generated ethereal user
-            pass: "turlam3M&Msy" // generated ethereal password
+            pass: "*" // generated ethereal password
         },
         tls: {
             rejectUnauthorized: false
